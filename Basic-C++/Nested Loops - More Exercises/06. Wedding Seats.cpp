90962a81a4e50e8d6dcb7a519e295086069ce29d
Jordan.Ganchev <jordan.ganchev.1434@gmail.com>
2025-05-05 12:38:50 +0300

    exercise

--- Basic-C++/Nested Loops - More Exercises/06. Wedding Seats.cpp
+++ Basic-C++/Nested Loops - More Exercises/06. Wedding Seats.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main() {
+    char lastSector;
+    int initialRows, seatsInOddRow;
 
-    
+    cin >> lastSector >> initialRows >> seatsInOddRow;
+    int totalSeats = 0;
+
+    for (char sector = 'A'; sector <= lastSector; sector++) {
+        int rowsInSector = initialRows + (sector - 'A');
+        for (int row = 1; row <= rowsInSector; row++) {
+            int seatsInRow = (row % 2 == 0) ? seatsInOddRow + 2 : seatsInOddRow;
+            for (char seat = 'a'; seat < 'a' + seatsInRow; seat++) {
+                cout << sector << row << seat << endl;
+                totalSeats++;
+            }
+        }
+    }
+
+    cout << totalSeats << endl;
 
     return 0;
 }
